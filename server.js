df584a334709d2d47255233cc25b90da9fa1b183
akira669 <akirauchiha0@gmail.com>
2018-04-16 20:28:29 -0500

    feature(server.js): modify server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 const path = require('path');
 const env = require('dotenv').config();
 const express = require('express');
+const chalk = require('chalk');
+const log = console.log;
 
 let server = express();
 
 server.use(express.static(path.join(__dirname + process.env.PATH)));
 
 server.listen(process.env.PORT);
-console.log(`Server is running on: http://127.0.0.1:${process.env.PORT}`);
+log(chalk.blue(`Server is running on: http://127.0.0.1:${process.env.PORT}`));

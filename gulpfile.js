ed9269c5430a95de9e494dfa438bd3c1be6e6057
carlos.buendia <juan.carlosutninfo@gmail.com>
2018-04-17 21:41:00 -0500

    modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const gulp = require('gulp');
 const template = require('gulp-template');
-const rename = require('gulp-rename');
+const rename  = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const argv = require('yargs').argv;
 
@@ -18,31 +18,35 @@ const argv = require('yargs').argv;
 * @desc Configure file of enviorement
 * @file .env
 */
+
 let env = argv.config ;
 
 /*
  * @desc assignation of environment variables obtained from the command
  * @params --config with yargs
  * */
-gulp.task('config', () => {
+gulp.task('config', function () {
+
     'use strict';
-    gulp.src(`env/${env}.config`)
-        .pipe(template())
+ return  gulp.src(`env/${env}.config`)
         .pipe(rename('.env'))
+        .pipe(template())
         .pipe(gulp.dest(''));
+   done();
 });
 
 
 /**
  * @desc gulp default triggers variable levnations and begin the survey of the app with browserSync
  * @param argv  - variable of enviorement
- */
-gulp.task('default', ['config'], () => {
+*/
+gulp.task('default', ['config'], function () {
+
     'use strict';
-    browserSync.init({
+   return browserSync.init({
         server: {
             baseDir: './'
-        },
+       },
         port:  process.env.PORT
     });
 });
@@ -50,8 +54,8 @@ gulp.task('default', ['config'], () => {
 /*
 * @desc browserSync Reload Daemon
 * @params config
-* */
-gulp.task('server-watch', ['config'], (done) => {
+*/
+gulp.task('server-watch', ['config'], function () {
     'use strict';
     browserSync.reload();
     done();

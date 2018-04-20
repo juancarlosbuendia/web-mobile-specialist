a01904c5f08972ff41309ae8e3f9fc48ce371f56
carlos.buendia <juan.carlosutninfo@gmail.com>
2018-04-19 20:50:26 -0500

    modify package.josn and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,43 @@
-'use strict';
-
-/****************
- *   Gulp : Default
- *   message : Survey of application in development or production environment
- *   created : full-stack team  more detail in README.md
- */
-
-require('dotenv').config();
-
 const gulp = require('gulp');
+
 const template = require('gulp-template');
-const rename  = require('gulp-rename');
-const browserSync = require('browser-sync').create();
 const argv = require('yargs').argv;
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
-/*
-* @desc Configure file of enviorement
-* @file .env
-*/
+const env = (argv.env === undefined) ? 'dev' : argv.env;
 
-let env = argv.config ;
 
-/*
- * @desc assignation of environment variables obtained from the command
- * @params --config with yargs
- * */
-gulp.task('config', function () {
+// Begins enviroment config
 
+gulp.task('config', () => {
     'use strict';
- return  gulp.src(`env/${env}.config`)
-        .pipe(rename('.env'))
+    gulp.src(`env/${env}.config`)
         .pipe(template())
+        .pipe(rename('.env'))
         .pipe(gulp.dest(''));
-   done();
 });
 
+// Ends enviroment config
 
-/**
- * @desc gulp default triggers variable levnations and begin the survey of the app with browserSync
- * @param argv  - variable of enviorement
-*/
-gulp.task('default', ['config'], function () {
+// Begins BrowserSync
 
-    'use strict';
-   return browserSync.init({
-        server: {
-            baseDir: './'
-       },
-        port:  process.env.PORT
-    });
-});
-
-/*
-* @desc browserSync Reload Daemon
-* @params config
-*/
-gulp.task('server-watch', ['config'], function () {
+gulp.task('server-watch', ['config'], (done) => {
     'use strict';
     browserSync.reload();
     done();
 });
 
+gulp.task('default', ['config'], () => {
+    'use strict';
+    browserSync.init({
+        server: {
+            baseDir: './',
+
+        },
+    });
+});
 
 gulp.watch('src/**/*.*', ['server-watch']);
+
+// Ends BrowserSync
